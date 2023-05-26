14d27592207e65a2b434eb9db74d4cc6ebd25fc1
dennis-22-csc <dennisakpotaire@gmail.com>
2023-05-26 21:46:40 +0100

    implemented error validation

--- frontend/lib/airtime_convert_form.dart
+++ frontend/lib/airtime_convert_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter/services.dart';
 
 class AirtimeConvert extends StatefulWidget {
   const AirtimeConvert({required Key key}) : super(key: key);
@@ -15,9 +16,24 @@ class AirtimeConvertState extends State<AirtimeConvert> {
   String _amount = '# 0.00';
   String _transactionFee = 'N/A';
   String _total = '# 0.00';
-  
+  final _phoneNumberFocusNode = FocusNode();
+  final _confirmPhoneNumberFocusNode = FocusNode();
+  final _amountFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _phoneNumberController.dispose();
+    _confirmPhoneNumberController.dispose();
+    _amountController.dispose();
+    _phoneNumberFocusNode.dispose();
+    _confirmPhoneNumberFocusNode.dispose();
+    _amountFocusNode.dispose();
+    super.dispose();
+  }
+ 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -33,7 +49,7 @@ class AirtimeConvertState extends State<AirtimeConvert> {
         padding: const EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
-          autovalidateMode: AutovalidateMode.onUserInteraction,
+          autovalidateMode: AutovalidateMode.disabled,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
@@ -51,11 +67,27 @@ class AirtimeConvertState extends State<AirtimeConvert> {
                       ),
                     ),
                   ),
+                  
+                  //keyboardType: TextInputType.number,
+                    /*inputFormatters:  <TextInputFormatter> [
+                        FilteringTextInputFormatter.allow(RegExp(r"^\+?0[0-9]{10}$")),
+                        FilteringTextInputFormatter.digitsOnly,                          
+                        ],*/
+                  validator: (value) {
+                    if (value == null || value.isEmpty){
+                      return "This field is required";
+                    }
+                    return null;
+                  },
                   onChanged: (value) {
                       setState(() {
                         _phoneNumber = value;
                       });
                   },
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context).requestFocus(_confirmPhoneNumberFocusNode);
+                  },
+                  focusNode: _phoneNumberFocusNode
                 ),
               ),
 
@@ -68,26 +100,52 @@ class AirtimeConvertState extends State<AirtimeConvert> {
                     suffixIcon: Icon(Icons.phone),
                     border: OutlineInputBorder(
                       borderSide: BorderSide(
-                        color: Colors.red,
+                        color:  Colors.red,
                         width: 2.0,
                       ),
                     ),
-                  ),  
+                  ), 
+                  
+                  //keyboardType: TextInputType.number,
+                    /*inputFormatters:  <TextInputFormatter> [
+                        FilteringTextInputFormatter.allow(RegExp(r"^\+?0[0-9]{10}$")),
+                        FilteringTextInputFormatter.digitsOnly,                          
+                        ],*/
+                  validator: (value) {
+                    if (value == null || value.isEmpty){
+                      return "This field is required";
+                    }
+                    if (value != _phoneNumberController.text) {
+                      return "Phone numbers doesn't match";
+                    }
+                    return null;
+                  },
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context).requestFocus(_amountFocusNode);
+                  },
+                  focusNode: _confirmPhoneNumberFocusNode,
                 ),
               ),
               Container(
                 margin: const EdgeInsets.only(bottom: 16.0),
-              child: TextFormField(
+                child: TextFormField(
                   controller: _amountController,
                   decoration: const InputDecoration(
                     hintText: "Amount",
                     border: OutlineInputBorder(
                       borderSide: BorderSide(
-                        color: Colors.red,
+                        color:  Colors.red,
                         width: 2.0,
                       ),
                     ),
-                  ),
+                  ), 
+                  
+                  validator: (value) {
+                    if (value == null || value.isEmpty){
+                      return "This field is required";
+                    }
+                    return null;
+                  },
                   onChanged: (value) {
                     setState(() {
                         _amount = '# $value';
@@ -95,6 +153,7 @@ class AirtimeConvertState extends State<AirtimeConvert> {
                         _total = '# ${getTotal(double.parse(value), getTransactionFee(double.parse(value)))}';
                       });
                   }, 
+                  focusNode: _amountFocusNode,
                 ),
               ),
               Container(
@@ -176,7 +235,25 @@ class AirtimeConvertState extends State<AirtimeConvert> {
                 margin: const EdgeInsets.only(bottom: 16.0),
                 child: ElevatedButton(
                   onPressed: () {
-      
+                    if (_formKey.currentState!.validate()) {
+                      showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text('Hi Chief!'),
+                          content: const Text('Your airtime conversion has been proccessed'),
+                          actions: [
+                            TextButton(
+                              child: const Text('OK'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                    } 
                   },
                   child: const Text('Submit'),
                 ),
@@ -194,4 +271,6 @@ class AirtimeConvertState extends State<AirtimeConvert> {
   double getTotal(double amount, double transactionFee){
     return amount + transactionFee;
   }
+  
+
 }
